8327aba5b577b22d3d9b95c242671add8896829b
sahanac1811 <sahana.c@knowledgelens.com>
2024-09-02 02:37:38 +0530

    saving 1st  final version

--- app/services/item_service.py
+++ app/services/item_service.py
@@ -1,6 +1,7 @@
 from pymongo.collection import Collection
 from app.schemas import ItemCreate, ItemResponse
 
+
 def create_item(item: ItemCreate, user_id: str, collection: Collection) -> dict:
     """
     Create a new item and store it in the collection.
@@ -11,7 +12,17 @@ def create_item(item: ItemCreate, user_id: str, collection: Collection) -> dict:
         "owner_id": user_id
     }
     result = collection.insert_one(db_item)
-    return ItemResponse(**db_item)
+
+    # Adding the inserted ID to the response
+    db_item["_id"] = str(result.inserted_id)  # Convert ObjectId to string
+
+    # Return the item in a format expected by the frontend
+    return {
+        "id": db_item["_id"],
+        "name": db_item["name"],
+        "price": db_item["price"],
+        "owner_id": db_item["owner_id"]
+    }
 
 
 def get_items_for_user(user_id: int, collection: Collection) -> list:
